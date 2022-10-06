f92f093e02abe74c5dd68cd23e5cd00c52df7d53
max65536 <maxinteger65536@gmail.com>
2022-10-06 11:18:14 +0200

    finish all steps

--- termin_spider/spiders/scan_spider.py
+++ termin_spider/spiders/scan_spider.py
@@ -1,6 +1,9 @@
 from http import cookies
 from scrapy.spiders import Spider
 from scrapy.utils.response import open_in_browser
+from scrapy import Request
+from tkinter import messagebox
+import requests
 from IPython import embed
 
 class ScanSpider(Spider):
@@ -9,17 +12,24 @@ class ScanSpider(Spider):
         'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
         
     }
-    cookie_dict = {
+    cookie_dict1 = {
         "cookie_accept":1,
-        "TVWebSession":"0g9603741eit44bmg9bn72koa6"
+        "TVWebSession":"xxxxxxxxxx"
+    }
+
+    cookie_dict2 = {
+        "cookie_accept":1,
+        "TVWebSession":"xxxxxxxxxx"
     }
 
         
     # start_urls = ["https://termine.staedteregion-aachen.de/auslaenderamt/suggest?cnc-145=1"]
 
     def start_requests(self):
-        url = "https://termine.staedteregion-aachen.de/auslaenderamt/suggest?cnc-145=1"
-        yield Request(url, headers=self.headers, cookies=self.cookie_dict)
+        # url = "https://termine.staedteregion-aachen.de/auslaenderamt/suggest?cnc-145=1"
+        # yield Request(url, headers=self.headers, cookies=self.cookie_dict)
+        url = "https://termine.staedteregion-aachen.de/auslaenderamt/"
+        yield Request(url, headers=self.headers)
 
     # def get_cookie(self):
     #     https://termine.staedteregion-aachen.de/auslaenderamt/
@@ -28,9 +38,43 @@ class ScanSpider(Spider):
         # from scrapy.shell import inspect_response
         # inspect_response(response, self)
         # open_in_browser(response)
+
+
+        # for title in titles:
+        #     print(title.strip())
+        
+        
+        self.cookie = response.headers['Set-Cookie']
+
+        next_url = "https://termine.staedteregion-aachen.de/auslaenderamt/select2?md=1"
+        # embed()
+        self.cookie_dict1["TVWebSession"] = self.cookie.split()[0][13:-1].decode('utf-8')
+        yield Request(next_url, headers=self.headers, cookies=self.cookie_dict1, callback=self.parse_step2)
+
+        error = response.xpath('//h1[@class="error"]').extract()
+        data = response.xpath('//div[@id="inhalt"]/h1').extract()
+        print("---------------------------data-----------------------------\n", data)
+
+    def parse_step2(self,response):
+        # with open("response.html", "w") as f:
+        #     f.write(response.text)
+        self.cookie = response.headers['Set-Cookie']
+        self.cookie_dict2["TVWebSession"] = self.cookie.split()[0][13:-1].decode('utf-8')
+        next_url = "https://termine.staedteregion-aachen.de/auslaenderamt/suggest?cnc-145=1&loc=16"
+        yield Request(next_url, headers=self.headers, cookies=self.cookie_dict2, callback=self.parse_step3)
+        data = response.xpath('//div[@id="inhalt"]/h1').extract()
+        print("---------------------------data-----------------------------\n", data)
+
+    def parse_step3(self, response):        
+        error = response.xpath('//h1[@class="error"]').extract()
+        data = response.xpath('//div[@id="inhalt"]/h1/text()').extract()
+        print("---------------------------data-----------------------------\n", data)
+        print("---------------------------error-----------------------------\n", error)
         with open("response.html", "w") as f:
             f.write(response.text)
-
-        titles = response.xpath('//a[@class="post-title-link"]/text()').extract()
-        for title in titles:
-            print(title.strip())
+        if "Keine" in data[0]:
+            # messagebox.showinfo("test")
+            pass
+        else:
+            messagebox.showinfo("New Termin!")
+        # embed()
